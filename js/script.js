9e854db820ba68d8599f24c81f8737b24a4e41c2
FedeAle1525 <fedeale.1525@gmail.com>
2023-02-21 16:36:23 +0100

    VUE for "Milestone 1"

--- js/script.js
+++ js/script.js
@@ -3,8 +3,33 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
-      }
+      tasks: [
+        {
+          text: "Prepararsi per la lezione",
+          done: true
+        },
+
+        {
+          text: "Andare in Banca",
+          done: true
+        },
+
+        {
+          text: "Fare la spesa",
+          done: true
+        },
+
+        {
+          text: "Fare esercizione pomeridiano",
+          done: false
+        },
+
+        {
+          text: "Preparare la cena",
+          done: false
+        }
+      ]
+    }
   }
 
 }).mount('#app')
